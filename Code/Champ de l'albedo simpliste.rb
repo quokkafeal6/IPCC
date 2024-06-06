8b365f9253b01a92782412b502c117b9f0462e17
Pierregb1 <pierre.garciab@gmail.com>
2024-06-06 17:44:54 +0200

    add code champ albedo
    code du champ de l'albedo simpliste

---
+++ Code/Champ de l'albedo simpliste.rb
@@ -0,0 +1,44 @@
+# Dictionnaire des valeurs d'albédo pour chaque partie de la Terre
+data_albedo = {
+    # Continents
+    "Amérique": 0.25,
+    "Europe": 0.3,
+    "Asie": 0.2,
+    "Afrique": 0.35,
+    # Océans
+    "Océans": 0.12,
+    # Pôles
+    "Pole Nord": 0.75,
+    "Pole Sud": 0.8
+}
+
+# Fonction pour déterminer la partie de la Terre en fonction des coordonnées de latitude et de longitude
+def determiner_partie_terre(latitude, longitude):
+    if latitude >= 60:  # Pole Nord
+        return "Pole Nord"
+    elif latitude <= -60:  # Pole Sud
+        return "Pole Sud"
+    elif 45 <= latitude <= 70 and -180 <= longitude <= -30:  # Europe
+        return "Europe"
+    elif -60 <= latitude <= 10 and -120 <= longitude <= -30:  # Amérique
+        return "Amérique"
+    elif -60 <= latitude <= 40 and -30 <= longitude <= 160:  # Asie
+        return "Asie"
+    elif -60 <= latitude <= 40 and -30 <= longitude <= 40:  # Afrique
+        return "Afrique"
+    else:
+        return "Océans"
+
+# Fonction pour obtenir l'albédo en fonction des coordonnées de latitude et de longitude
+def obtenir_albedo(latitude, longitude):
+    partie_terre = determiner_partie_terre(latitude, longitude)
+    return data_albedo[partie_terre]
+
+# Coordonnées de latitude et de longitude en entrée
+latitude_entree = float(input("Entrez la latitude : "))
+longitude_entree = float(input("Entrez la longitude : "))
+
+# Obtention de l'albédo pour les coordonnées spécifiées
+albedo = obtenir_albedo(latitude_entree, longitude_entree)
+
+print("L'albédo est de :", albedo)
