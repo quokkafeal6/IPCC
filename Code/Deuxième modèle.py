2b4cd7d65089b060153b969d91754e34e2668346
misterredmagic <michelsvic@gmail.com>
2024-06-06 16:34:38 +0200

    Code Deuxième model

---
+++ Code/Deuxième modèle.py
@@ -0,0 +1,37 @@
+# Import necessary library
+import math
+import numpy as np
+
+# Constantes
+S = 1361  # Constante solaire en W/m^2
+A = 0.3  # Albédo terrestre
+sigma = 5.67e-8  # Constante de Stefan-Boltzmann en W/m^2/K^4
+
+def calculate_temperature(S, A, sigma, lat):
+    """
+    Calcule la température d'équilibre radiatif de la Terre.
+
+    :param S: Constante solaire en W/m^2
+    :param A: Albédo terrestre
+    :param sigma: Constante de Stefan-Boltzmann en W/m^2/K^4
+    :return: Température d'équilibre en Kelvin
+    """
+    Slat = S*np.cos(lat*2*3.14/360)
+    # Énergie moyenne reçue par unité de surface
+    energy_received = (1 - A) * Slat / 4
+
+    # Température d'équilibre
+    T = (energy_received / sigma) ** 0.25
+    return T
+
+# Calcul de la température moyenne de la Terre
+lat=-1
+while lat<0:
+    lat = int(input("entrer la latitude "))
+
+T_earth = calculate_temperature(S, A, sigma, lat)
+print(f"La température moyenne de la Terre est de {T_earth:.2f} K")
+
+# Convertir en Celsius
+T_earth_Celsius = T_earth - 273.15
+print(f"Ce qui correspond à {T_earth_Celsius:.2f} °C")
