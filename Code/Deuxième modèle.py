88c9cb2f9e39e0aaaad2e46f8d0a2d3fe3ab52e8
Picpoule <lisepradier04@gmail.com>
2024-06-06 17:12:02 +0200

    Update Deuxième modèle.py

--- Code/Deuxième modèle.py
+++ Code/Deuxième modèle.py
@@ -1,4 +1,3 @@
-# Import necessary library
 import math
 import numpy as np
 
@@ -7,29 +6,43 @@ S = 1361  # Constante solaire en W/m^2
 A = 0.3  # Albédo terrestre
 sigma = 5.67e-8  # Constante de Stefan-Boltzmann en W/m^2/K^4
 
-def calculate_temperature(S, A, sigma, lat):
+def calculate_temperature(S, A, sigma, lat, lon):
     """
     Calcule la température d'équilibre radiatif de la Terre.
 
     :param S: Constante solaire en W/m^2
     :param A: Albédo terrestre
     :param sigma: Constante de Stefan-Boltzmann en W/m^2/K^4
+    :param lat: Latitude en degrés
     :return: Température d'équilibre en Kelvin
     """
-    Slat = S*np.cos(lat*2*3.14/360)
-    # Énergie moyenne reçue par unité de surface
-    energy_received = (1 - A) * Slat / 4
+    # Vérifier que la latitude est dans la plage valide
+    if lat < -90 or lat > 90:
+        raise ValueError("La latitude doit être comprise entre -90 et 90 degrés.")
+    elif lon < -90 or lon > 90:
+        raise ValueError("La longitude doit être comprise entre -90 et 90 degrés.")
 
-    # Température d'équilibre
+    # Calcul de l'énergie reçue en fonction de la latitude
+    Slatlon = S * np.cos(lat * 2 * math.pi / 360)*np.sin(lon*2*math.pi/360)
+
+    # Énergie moyenne reçue par unité de surface
+    energy_received = (1 - A) * Slatlon / 4
+
+    # Température d'équilibre10
     T = (energy_received / sigma) ** 0.25
     return T
 
-# Calcul de la température moyenne de la Terre
-lat = int(input("entrer la latitude "))
+try:
+    # Entrer la latitude
+    lat = int(input("Entrer la latitude (entre -90 et 90 degrés) : "))
+    lon = int(input("Entrer la longitude (entre -90 et 90 degrés) : "))
 
-T_earth = calculate_temperature(S, A, sigma, lat)
-print(f"La température moyenne de la Terre est de {T_earth:.2f} K")
+    # Calcul de la température moyenne de la Terre
+    T_earth = calculate_temperature(S, A, sigma, lat,lon)
+    print(f"La température moyenne de la Terre à la latitude {lat}° et la longitude {lon}°est de {T_earth:.2f} K")
 
-# Convertir en Celsius
-T_earth_Celsius = T_earth - 273.15
-print(f"Ce qui correspond à {T_earth_Celsius:.2f} °C")
+    # Convertir en Celsius
+    T_earth_Celsius = T_earth - 273.15
+    print(f"Ce qui correspond à {T_earth_Celsius:.2f} °C")
+except ValueError as e:
+    print(e)
